b67acb4f13bbf21515f153b9f8be04e62b3e984f
Abdallah <abdallah.ashraf854@gmail.com>
2026-06-12 12:04:44 +0300

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -89,7 +89,7 @@ const PROJECTS = [
     tags:    ['CNN', 'LSTM', 'PyTorch', 'Temporal model'],
     bullets: [
       'In group activity recognition, the temporal dynamics of the whole activity can be inferred based on the dynamics of the individual people representing the activity.',
-      'I will build a deep model to capture these edynamics based on LSTM models',
+      'I will build a deep model to capture these dynamics based on LSTM models',
       'LSTM model will be designed to represent action dynamics of individual people in a sequence and another LSTM model will be designed to aggregate person-level information for whole activity understanding.'
     ],
     link: 'https://github.com/Abd-Salem/group-activity-recognition',
